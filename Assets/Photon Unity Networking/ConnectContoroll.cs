a79529d84c40b800e9f41652828be6aadab8477a
StreinGon <artyom199788@yandex.by>
2018-07-05 11:51:13 +0300

    fhoton v1 multi added

---
+++ Assets/Photon Unity Networking/ConnectContoroll.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConnectContoroll : Photon.MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		PhotonNetwork.ConnectUsingSettings ("v1");
+	}
+	void OnJoinedLobby() {
+		PhotonNetwork.JoinOrCreateRoom ("VA", new RoomOptions (), TypedLobby.Default);
+	}
+	void OnJoinedRoom(){
+		Vector3 spawn=new Vector3(0,20,-686);
+
+		PhotonNetwork.Instantiate("fighter01",spawn,Quaternion.Euler(0,180,0),0);
+	}
+}
--- Assets/Photon Unity Networking/Player.cs
+++ Assets/Photon Unity Networking/Player.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
+public class Player : Photon.MonoBehaviour {
 
     private Rigidbody rb;
     public float speed;
@@ -13,20 +13,47 @@ public class Player : MonoBehaviour {
     public float fireRate;
     public GameObject TouchPad;
     private float nextFire;
-
+	CharacterController controller;
     private void Start()
     {
+		controller = GetComponent<CharacterController> ();
         rb = GetComponent<Rigidbody>();
     }
+	void OnPhotonSerializeView(PhotonStream stream,PhotonMessageInfo info){
+		Vector3 pos=transform.position;
+		Quaternion rot= transform.rotation;
+		stream.Serialize(ref pos);
+		stream.Serialize(ref rot);
+		if(stream.isReading){
+			transform.position=pos;
+			transform.rotation=rot;
+		}
+	}
     private void Update()
     {
-        //if (Input.GetKeyDown(KeyCode.Space))
-        if(Time.time > nextFire)
-        {
-            nextFire = Time.time + fireRate;
-            Instantiate(ammo, new Vector3(shotSpawn.transform.position.x, 36, shotSpawn.transform.position.z) , Quaternion.Euler(0, 0, 0));
-        }
-
+		Vector3 move = Vector3.zero;
+		if(photonView.isMine){
+			move = new Vector3(Input.GetAxis("Horizontal") * speed * 1000 * Time.deltaTime, 0, Input.GetAxis("Vertical") * speed * 1000 * Time.deltaTime);
+			rb.rotation = Quaternion.Euler(rb.velocity.z * -tilty, 180, rb.velocity.x * tiltx);
+			if ((transform.position.x < -200))
+			{
+				transform.position = new Vector3(-200, transform.position.y, transform.position.z);
+			}
+			if ((transform.position.x > 200))
+			{
+				transform.position = new Vector3(200, transform.position.y, transform.position.z);
+			}
+			if ((transform.position.z < -781))
+			{
+				transform.position = new Vector3(transform.position.x, transform.position.y, -781);
+			}
+			rb.velocity = move;
+		}
+		if(Time.time > nextFire && Input.GetKeyDown(KeyCode.Space))
+		{
+			nextFire = Time.time + fireRate;
+			Instantiate(ammo, new Vector3(shotSpawn.transform.position.x, 36, shotSpawn.transform.position.z) , Quaternion.Euler(0, 0, 0));
+		}
     }
     private void FixedUpdate()
     {
