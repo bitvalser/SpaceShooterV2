cc5342c6bb1631bdec0dcd4dbb1fae32e2f03bc9
StreinGon <artyom199788@yandex.by>
2018-07-05 20:06:03 +0300

    arena v1 , huevaia camera

---
+++ Assets/pvp/ConnectControllPVP.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConnectControllPVP : Photon.MonoBehaviour {
+	private bool isGame;
+	void Start()
+	{
+		isGame = false;
+		PhotonNetwork.ConnectUsingSettings("v1");
+
+	}
+	void OnJoinedLobby() {
+		PhotonNetwork.JoinOrCreateRoom ("VA", new RoomOptions (), TypedLobby.Default);
+
+	}
+	void OnJoinedRoom(){
+		if (PhotonNetwork.countOfPlayersInRooms < 1) {
+			Vector3 spawn = new Vector3 (266, 130, -2805);
+			PhotonNetwork.Instantiate ("fighterPVP1", spawn, Quaternion.Euler (0, 180, 0), 0);
+		}
+		if (PhotonNetwork.countOfPlayersInRooms > 1) {
+			Vector3 spawn = new Vector3 (193, 130, -409);
+			PhotonNetwork.Instantiate ("fighterPVP2", spawn, Quaternion.Euler (0, 180, 0), 0);
+		}
+	}
+}
---
+++ Assets/pvp/DestroyByPVP.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByPVP : MonoBehaviour {
+
+	public GameObject explosion;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if(other.tag == "Player")
+		{
+			return;
+		}
+		Instantiate(explosion, transform.position, transform.rotation);
+		Destroy(other.gameObject);
+
+		if(other.tag == "Enemy")
+		{
+			Instantiate(explosion, other.transform.position, other.transform.rotation);
+		}
+		Destroy(gameObject);
+	}
+}
