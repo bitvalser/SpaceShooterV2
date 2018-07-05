188d441134f7419dc24bae89618bfb9d2647624d
StreinGon <artyom199788@yandex.by>
2018-07-05 11:58:50 +0300

    fire fixed

--- Assets/Photon Unity Networking/Player.cs
+++ Assets/Photon Unity Networking/Player.cs
@@ -11,12 +11,12 @@ public class Player : Photon.MonoBehaviour {
     public GameObject shotSpawn;
     public GameObject ammo;
     public float fireRate;
-    public GameObject TouchPad;
+    //public GameObject TouchPad;
     private float nextFire;
-	CharacterController controller;
+	//CharacterController controller;
     private void Start()
     {
-		controller = GetComponent<CharacterController> ();
+		//controller = GetComponent<CharacterController> ();
         rb = GetComponent<Rigidbody>();
     }
 	void OnPhotonSerializeView(PhotonStream stream,PhotonMessageInfo info){
@@ -48,34 +48,35 @@ public class Player : Photon.MonoBehaviour {
 				transform.position = new Vector3(transform.position.x, transform.position.y, -781);
 			}
 			rb.velocity = move;
+			if(Time.time > nextFire && Input.GetKeyDown(KeyCode.Space))
+			{
+				nextFire = Time.time + fireRate;
+				Instantiate(ammo, new Vector3(shotSpawn.transform.position.x, 36, shotSpawn.transform.position.z) , Quaternion.Euler(0, 0, 0));
+			}
 		}
-		if(Time.time > nextFire && Input.GetKeyDown(KeyCode.Space))
-		{
-			nextFire = Time.time + fireRate;
-			Instantiate(ammo, new Vector3(shotSpawn.transform.position.x, 36, shotSpawn.transform.position.z) , Quaternion.Euler(0, 0, 0));
-		}
-    }
-    private void FixedUpdate()
-    {
-        Vector3 move = Vector3.zero;
 
-        Vector2 direct = TouchPad.GetComponent<TouchPad>().getDirect();
-        move = new Vector3(direct.x * speed * 1000 * Time.deltaTime, 0, direct.y * speed * 1000 * Time.deltaTime);
-
-        move = new Vector3(Input.GetAxis("Horizontal") * speed * 1000 * Time.deltaTime, 0, Input.GetAxis("Vertical") * speed * 1000 * Time.deltaTime);
-        rb.rotation = Quaternion.Euler(rb.velocity.z * -tilty, 180, rb.velocity.x * tiltx);
-        if ((transform.position.x < -200))
-        {
-            transform.position = new Vector3(-200, transform.position.y, transform.position.z);
-        }
-        if ((transform.position.x > 200))
-        {
-            transform.position = new Vector3(200, transform.position.y, transform.position.z);
-        }
-        if ((transform.position.z < -781))
-        {
-            transform.position = new Vector3(transform.position.x, transform.position.y, -781);
-        }
-        rb.velocity = move;
     }
+//    private void FixedUpdate()
+//    {
+//        Vector3 move = Vector3.zero;
+//
+//        Vector2 direct = TouchPad.GetComponent<TouchPad>().getDirect();
+//        move = new Vector3(direct.x * speed * 1000 * Time.deltaTime, 0, direct.y * speed * 1000 * Time.deltaTime);
+//
+//        move = new Vector3(Input.GetAxis("Horizontal") * speed * 1000 * Time.deltaTime, 0, Input.GetAxis("Vertical") * speed * 1000 * Time.deltaTime);
+//        rb.rotation = Quaternion.Euler(rb.velocity.z * -tilty, 180, rb.velocity.x * tiltx);
+//        if ((transform.position.x < -200))
+//        {
+//            transform.position = new Vector3(-200, transform.position.y, transform.position.z);
+//        }
+//        if ((transform.position.x > 200))
+//        {
+//            transform.position = new Vector3(200, transform.position.y, transform.position.z);
+//        }
+//        if ((transform.position.z < -781))
+//        {
+//            transform.position = new Vector3(transform.position.x, transform.position.y, -781);
+//        }
+//        rb.velocity = move;
+//    }
 }
